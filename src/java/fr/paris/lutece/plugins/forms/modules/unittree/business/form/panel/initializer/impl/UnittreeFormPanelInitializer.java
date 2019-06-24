b9a5fc408e3dd156ae5e72e142a799e4001184d9
Guillaume Lépine <guillaume.lepine@sogeti.com>
2019-06-24 15:07:37 +0200

    FORMSUT-3 : Create FormPanel configuration to filter forms response by unit

---
+++ src/java/fr/paris/lutece/plugins/forms/modules/unittree/business/form/panel/initializer/impl/UnittreeFormPanelInitializer.java
@@ -0,0 +1,6 @@
+package fr.paris.lutece.plugins.forms.modules.unittree.business.form.panel.initializer.impl;
+
+import fr.paris.lutece.plugins.forms.business.form.panel.initializer.impl.AbstractFormPanelInitializer;
+
+public class UnittreeFormPanelInitializer extends AbstractFormPanelInitializer {
+}
