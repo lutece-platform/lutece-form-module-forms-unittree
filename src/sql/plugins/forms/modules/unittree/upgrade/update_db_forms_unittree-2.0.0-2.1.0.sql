07fd6185235928964c1b76c4bbb0cab21233d4af
jlf-eviden <jlf-eviden@DESKTOP-BCRT9V4>
2025-07-25 11:48:26 +0200

    LUT-30429 : Adding liquibase tags to SQL scripts (Lutèce 7)

--- src/sql/plugins/forms/modules/unittree/upgrade/update_db_forms_unittree-2.0.0-2.1.0.sql
+++ src/sql/plugins/forms/modules/unittree/upgrade/update_db_forms_unittree-2.0.0-2.1.0.sql
@@ -1,2 +1,5 @@
+--liquibase formatted sql
+--changeset forms-unittree:update_db_forms_unittree-2.0.0-2.1.0.sql
+--preconditions onFail:MARK_RAN onError:WARN
 ALTER TABLE forms_unittree_unit_selection_config ADD COLUMN is_multiform SMALLINT DEFAULT 0 NOT NULL;
 ALTER TABLE forms_unittree_unit_selection_config_value ADD COLUMN code VARCHAR(100);
